82b943e703f17b2046b33efcb9d306dca4f7a09d
Tim Keenan <tkeenan911@gmail.com>
2020-03-21 13:34:52 -0400

    Adds 3D Spatial Background Music

---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioSource backgroundMusic;
+    private bool songSwitch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        songSwitch = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            songSwitch = !songSwitch;
+            if (songSwitch)
+            {
+                backgroundMusic.Play();
+            }
+            else
+            {
+                backgroundMusic.Stop();
+            }
+        }
+    }
+}

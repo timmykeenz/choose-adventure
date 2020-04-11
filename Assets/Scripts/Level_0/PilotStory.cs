04fa4b099ca73356fa646b921768a51c17b4b96e
Tim Keenan <tkeenan911@gmail.com>
2020-04-10 22:42:49 -0400

    Adds basic story script and doors that need cleaning

---
+++ Assets/Scripts/Level_0/PilotStory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilotStory : MonoBehaviour
+{
+    private bool explodedWalls = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if the object exists
+        if (CameraUse.objectToUse && CameraUse.isUsing && !explodedWalls)
+        {
+            if (CameraUse.objectToUse.tag.Equals("Radio"))
+            {
+                explodedWalls = true;
+                GameObject[] walls = GameObject.FindGameObjectsWithTag("ExplodingWall");
+                //Loop through walls
+                for (int i = 0; i < walls.Length; i++)
+                {
+                    walls[i].GetComponent<Rigidbody>().isKinematic = false;
+                    walls[i].GetComponent<Rigidbody>().AddRelativeForce(0, 0, 1000);
+                }
+            }
+        }
+    }
+}

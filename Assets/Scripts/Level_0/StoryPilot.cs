fd2d79a72a1085dc611b8b92fa8ea8bfe9922826
Tim Keenan <tkeenan911@gmail.com>
2020-04-11 23:09:05 -0400

    Battery holder now activates gate

---
+++ Assets/Scripts/Level_0/StoryPilot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryPilot : MonoBehaviour
+{
+    public GameObject gate;
+    public static bool entryGateOpened = false;
+
+    void Update()
+    {
+        if (PlaceBattery.isConnected && !entryGateOpened)
+        {
+            entryGateOpened = true;
+            gate.GetComponentInChildren<Animator>().Play("OpenGate");
+        }
+    }
+}

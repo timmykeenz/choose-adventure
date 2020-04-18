8ad2f33bd0af17fa7aa798410ea444d703c6d367
Tim Keenan <tkeenan911@gmail.com>
2020-04-18 11:15:47 -0400

    Adds helper text for battery holder and QOL improvements

--- Assets/Scripts/Level_0/StoryPilot.cs
+++ Assets/Scripts/Level_0/StoryPilot.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class StoryPilot : MonoBehaviour
 {
     public GameObject gate;
-    public static bool entryGateOpened = false;
-    public static bool hasGrabbedBattery = false;
+    public GameObject batteryHolderText;
+    public static bool entryGateOpened;
+    public static bool hasGrabbedBattery;
+    private static bool showHolderText;
+
+    private void Start()
+    {
+        entryGateOpened = false;
+        hasGrabbedBattery = false;
+        showHolderText = false;
+    }
 
     void Update()
     {
         if (PlaceBattery.isConnected && !entryGateOpened)
         {
+            showHolderText = false;
             entryGateOpened = true;
             gate.GetComponentInChildren<Animator>().Play("OpenGate");
+        }
+        //Check if we should show the text for the battery holder
+        if (showHolderText)
+        {
+            batteryHolderText.SetActive(true);
+        //If not, make it invisible
+        } else
+        {
+            batteryHolderText.SetActive(false);
         }
 
         //Check if we are grabbing an object
         if (CameraGrab.isGrabbing)
         {
+            if (!PlaceBattery.isConnected)
+            {
+                showHolderText = true;
+            }
             //Check if the user has grabbed a battery yet
             if (CameraGrab.objectGrabbed.tag.Equals("Battery") && !hasGrabbedBattery)
             {

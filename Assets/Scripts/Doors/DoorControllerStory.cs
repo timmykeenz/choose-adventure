af7f091f93fc5023ea3fe4edcdb3e1fcb11294f2
Tim Keenan <tkeenan911@gmail.com>
2020-04-18 16:25:44 -0400

    Add new script

---
+++ Assets/Scripts/Doors/DoorControllerStory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControllerStory : MonoBehaviour
+{
+    private bool doorActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorActive = false;
+    }
+
+    //Runs when a collision is detected
+    private void OnTriggerStay(Collider other)
+    {
+        //Check that the player is what we are colliding with and not some object
+        if (other.tag.Equals("Player") && Input.GetKeyDown(CameraUse.usedKey))
+        {
+            doorActive = !doorActive;
+            //Looks for animator in object's children and set trigger
+            GetComponentInChildren<Animator>().SetTrigger("OpenClose");
+        }
+    }
+}

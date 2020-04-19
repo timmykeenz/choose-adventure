819182f40399a4ad2d3edcb295d9247783dc4fd3
Tim Keenan <tkeenan911@gmail.com>
2020-04-18 22:37:37 -0400

    Door GameObject enabling

--- Assets/Scripts/Level_0/StoryPilot.cs
+++ Assets/Scripts/Level_0/StoryPilot.cs
@@ -9,15 +9,29 @@ public class StoryPilot : MonoBehaviour
     public GameObject radioHelpText;
     public DoorController redDoor;
     public DoorController blueDoor;
+    public GameObject blueChoiceRoom;
+    public GameObject redChoiceRoom;
+    public float roomDeactivationTime;
+    private bool redRoomTrigger;
+    private bool blueRoomTrigger;
     public static bool entryGateOpened;
     public static bool hasGrabbedBattery;
     private static bool showHolderText;
 
     private void Start()
     {
+        //Disable gate at level open
         entryGateOpened = false;
+        //Disable battery grabbing so help text shows
         hasGrabbedBattery = false;
+        //Show holder text starts off by default
         showHolderText = false;
+        //Disable rooms behind doors
+        blueChoiceRoom.SetActive(false);
+        redChoiceRoom.SetActive(false);
+        //Setup triggers for the rooms (Don't touch these values)
+        redRoomTrigger = false;
+        blueRoomTrigger = false;
     }
 
     void Update()
@@ -30,21 +44,47 @@ public class StoryPilot : MonoBehaviour
         CheckDoors();
     }
     private void CheckDoors()
-    {
+    { 
+        //If blue door is open
         if (blueDoor.isOpen)
         {
+            //Disable red door and activate the room
             redDoor.SetUserCanOpen(false);
-        } else
+            blueChoiceRoom.SetActive(true);
+            //Setup trigger for deactivation
+            blueRoomTrigger = true;
+        } else if (blueRoomTrigger)
         {
+            //Reset trigger
+            blueRoomTrigger = false;
+            //Enable red door and deactivate the room on door close
             redDoor.SetUserCanOpen(true);
+            StartCoroutine(DisableGameObject(roomDeactivationTime, blueChoiceRoom));
         }
+        //If red door is open
         if (redDoor.isOpen)
         {
+            //Disable blue door and activate the room
             blueDoor.SetUserCanOpen(false);
-        } else
+            redChoiceRoom.SetActive(true);
+            //Setup trigger for deactivation
+            redRoomTrigger = true;
+        } else if (redRoomTrigger)
         {
+            //Reset trigger
+            redRoomTrigger = false;
+            //Enable blue door and deactivate the room on door close
             blueDoor.SetUserCanOpen(true);
-        }
+            StartCoroutine(DisableGameObject(roomDeactivationTime, redChoiceRoom));
+        } 
+    }
+    IEnumerator DisableGameObject(float time, GameObject obj)
+    {
+        //Make code wait for 'x' amount of time
+        yield return new WaitForSeconds(time);
+        print("Running!");
+        //Disable GameObject
+        obj.SetActive(false);
     }
     private void CheckBattery()
     {
